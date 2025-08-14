08c3642028370fde6cfc69dc6195238e88b223f4
Mokshit09 <123cs0172@nitrkl.ac.in>
2025-08-14 19:40:50 +0530

    Geometry objects completed <14/08/25>

--- venv/core/geometry/geometry.py
+++ venv/core/geometry/geometry.py
@@ -13,4 +13,26 @@ class Geometry(object):
         attrib = list(self.attributes.values())[0]
         self.vertexCount = len(attrib.data)
 
+    def applyMatrix(self, matrix, variableName="vertexPosition"):
+        oldPositionData = self.attributes[variableName].data
+        newPositionData = []
+
+        for oldPos in oldPositionData:
+            newPos = oldPos.copy()
+            #add homogenous fourth coordinate
+            newPos.append(1)
+            newPos = matrix @ newPos
+            #remove the fourth position 
+            newPos = list(newPos[0:3])
+            newPositionData.append(newPos)
+
+        self.attributes[variableName].data = newPositionData
+        self.attributes[variableName].uploadData()
+
+    def merge(self, otherGeometry):
+        for variablesName, attributeObject in self.attributes.items():
+            attributeObject.data += otherGeometry.attributes[variablesName].data
+
+            attributeObject.uploadData()
+        self.countVertices()
         
